c8970f6e499fd83d282610bd9b416fb2acebbf75
manuelots <47218143+manuelots@users.noreply.github.com>
2019-01-31 16:32:34 +0100

    Create webbook.php

---
+++ webbook.php
@@ -0,0 +1,19 @@
+<?php 
+define('BOT_TOKEN', '711717314:AAEeylWAVHUyr_O2YEN-Gpi01R7lq4GHS5U');
+define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
+	
+// read incoming info and grab the chatID
+$content = file_get_contents("php://input");
+$update = json_decode($content, true);
+$chatID = $update["message"]["chat"]["id"];
+		
+// compose reply
+$reply =  sendMessage();
+		
+// send reply
+$sendto =API_URL."sendmessage?chat_id=".$chatID."&text=".$reply;
+file_get_contents($sendto);
+
+function sendMessage(){
+$message = "I am a baby bot.";
+return $message;
